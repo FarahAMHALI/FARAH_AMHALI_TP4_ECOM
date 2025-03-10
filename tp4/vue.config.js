cfd6b8deabbe41af5a3db33486916b03e1614a89
FarahAMHALI <Farah.AMHALI@um6p.ma>
2025-03-10 23:56:49 +0000

    update

--- tp4/vue.config.js
+++ tp4/vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    client: {
+      webSocketURL: 'wss://0.0.0.0/ws', // Forces secure WebSocket (wss://)
+    },
+  },
+};
